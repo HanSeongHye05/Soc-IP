d198e0d823fca518ee8660b57412a5d946baaf66
HanSeongHye05 <hanseonghye0516@gmail.com>
2026-04-07 12:45:47 +0900

    Update exam03_var_module.v

--- winter_olympic_multiwatch/myip_stopwatch/exam03_var_module.v
+++ winter_olympic_multiwatch/myip_stopwatch/exam03_var_module.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/09/2025 09:38:53 AM
-// Design Name: 
-// Module Name: exam03_var_module
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 //module watch(

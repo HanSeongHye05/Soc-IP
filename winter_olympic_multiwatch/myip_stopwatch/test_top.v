f14e5e16e05ea4d163c36e31c746da1ec2c52149
HanSeongHye05 <hanseonghye0516@gmail.com>
2026-04-07 12:46:11 +0900

    Update test_top.v

--- winter_olympic_multiwatch/myip_stopwatch/test_top.v
+++ winter_olympic_multiwatch/myip_stopwatch/test_top.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/04/2025 09:16:19 AM
-// Design Name: 
-// Module Name: test_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 

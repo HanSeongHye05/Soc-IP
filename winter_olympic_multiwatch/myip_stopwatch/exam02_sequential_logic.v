7d23df93570bb0ba3e87692b2399c026276383ca
HanSeongHye05 <hanseonghye0516@gmail.com>
2026-04-07 12:45:18 +0900

    Update exam02_sequential_logic.v

--- winter_olympic_multiwatch/myip_stopwatch/exam02_sequential_logic.v
+++ winter_olympic_multiwatch/myip_stopwatch/exam02_sequential_logic.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/04/2025 02:06:42 PM
-// Design Name: 
-// Module Name: exam02_sequential_logic
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module D_flip_flop_n(
